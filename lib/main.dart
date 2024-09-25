afc6d7f2a0036217012c9e374b4f86cef4fa401c
asem-kurd <asem.vip10@gmail.com>
2024-09-25 19:57:13 +0300

    Video 4 Learn how to add a container and put on in its defails color, width, height etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(LearnFlutter()); // LearnFlutter() is a custum widget created by developer not build in prog_lang
+  runApp(
+      LearnFlutter()); // LearnFlutter() is a custum widget created by developer not build in prog_lang
 }
 
 class LearnFlutter extends StatelessWidget {
-  LearnFlutter(); // constructor
-  @override
   Widget build(BuildContext context) {
     // return type is Widget || return MaterialApp with its details
     return MaterialApp(
@@ -23,7 +22,14 @@ class LearnFlutter extends StatelessWidget {
           hoverColor: Colors.amber,
         ),
         body: Center(
-          child: Text("Hello AK"),
+          child: Container(
+            width: 93,
+            height: 83,
+            color: Colors.deepPurple,
+            child: Center(
+              child: Text("Hello AK"),
+            ),
+          ),
         ),
       ),
     );
