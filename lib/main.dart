19c286fd9f2bc2944f79ab36ff36575ab38e9b8f
asem-kurd <asem.vip10@gmail.com>
2024-09-25 15:32:58 +0300

    Video 2 add 1-Scaffold widget and add AppBar widget with title and backcolor 2-Add floatActionButton with hover effect

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello AK"),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(  
+            "Learn Flutter Application",
+          ),
+          backgroundColor: const Color.fromARGB(255, 111, 24, 24),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: const Color.fromARGB(255, 111, 24, 24),
+          hoverColor: Colors.amber,
+        ),
+        body: Center(
+          child: Text("Hello AK"),
+        ),
+      ),
     ),
-  ));
+  );
 }
