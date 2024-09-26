590411e1329a65b8ba9e9c7c4ed8fea378bfd33f
asem-kurd <asem.vip10@gmail.com>
2024-09-26 12:29:22 +0300

    Start BirthdayCard section of course 1-Delete every thing 2-Add new calss widget its name BirthdatCard 3-Add image on it after add it on yaml file in assets 4-Center every thing 5-After Center Add backColor to Scaffold widget to look like one image becose there are a spaces up and down after center the image

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      LearnFlutter()); // LearnFlutter() is a custum widget created by developer not build in prog_lang
+  runApp(BirthdayCard());
 }
 
-class LearnFlutter extends StatelessWidget {
+class BirthdayCard extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    // return type is Widget || return MaterialApp with its details
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Learn Flutter Application",
-          ),
-          backgroundColor: const Color.fromARGB(255, 111, 24, 24),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: const Color.fromARGB(255, 111, 24, 24),
-          hoverColor: Colors.amber,
-        ),
+        backgroundColor: Color(0xffd2bcd5),
         body: Center(
-          child: Container(
-            width: 93,
-            height: 83,
-            color: Colors.deepPurple,
-            child: Center(
-              child: Text("Hello AK"),
+          child: Image(
+            image: AssetImage(
+              "Images/birthdayCard.png",
             ),
           ),
         ),
