c320b6b5cadc6a1c779684e17becff219e418713
asem-kurd <asem.vip10@gmail.com>
2024-09-27 19:50:55 +0300

    New App BusnissCardApp 1-Deleta img from last app 2-Add three imgs logo call mail 3-Add folder font in it Pacifico font file 4-Update pub.yaml file 5-Delete widget_test.dart file 6-Add all the widgets in img 7-Update first & second widgets img & font

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,105 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(BirthdayCard());
+  runApp(BusnissCardApp());
 }
 
-class BirthdayCard extends StatelessWidget {
-  @override
+class BusnissCardApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xffd2bcd5),
-        body: Center(
-          child: Image(
-            image: AssetImage(
-              "Images/birthdayCard.png",
+        backgroundColor: Color(0xff23445d),
+        body: Column(
+          children: [
+            CircleAvatar(
+              radius: 122,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 120,
+                backgroundImage: AssetImage("Images/logo.png"),
+              ),
             ),
-          ),
+            Text(
+              "Asem Alkurdi",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 45,
+                fontFamily: "Pacifico",
+              ),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                color: const Color.fromARGB(255, 177, 171, 171),
+                fontSize: 23,
+              ),
+            ),
+            // Padding(
+            //   // padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+            // ),
+            Container(
+              width: 220,
+              height: 40,
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Image(
+                    image: AssetImage(
+                      "Images/call.png",
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  ),
+                  Text(
+                    "(+962) 797493629",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              width: 250,
+              height: 40,
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Image(
+                    image: AssetImage(
+                      "Images/mail.png",
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  ),
+                  Text(
+                    "asem.vip10@gmail.com",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
